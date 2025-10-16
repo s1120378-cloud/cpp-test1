85086216fa8573c3e2a45f9d67472c8a69a2e454
s1120378-cloud <s1120378@dmhs.kh.edu.tw>
2025-10-16 14:26:06 +0800

    第四次作業

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if (i % 7 == 0) {
-            cout << i << " ";
+    // First part: Multiplication Table using while loops
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j++;
         }
+        cout << endl;
+        i++;
     }
+
+    // Second part: Printing triangle pattern using for loops
+    cout << "\nTriangle Pattern:\n";
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
