b469397de020f6abb1ce49944c9454daf6fcccff
s1120378-cloud <s1120378@dmhs.kh.edu.tw>
2025-11-06 14:20:19 +0800

    homework7

--- main.cpp
+++ main.cpp
@@ -1,27 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 using namespace std;
 
 int main() {
-    // First part: Multiplication Table using while loops
-    int i = 1;
-    while (i <= 9) {
-        int j = 1;
-        while (j <= 9) {
-            cout << i << " * " << j << " = " << i * j << "\t";
-            j++;
-        }
-        cout << endl;
-        i++;
+    int stack[5];
+    int top = -1;  // 堆疊頂端指標
+
+    top++;
+    stack[top] = 10;
+
+    top++;
+    stack[top] = 20;
+
+    top++;
+    stack[top] = 30;
+
+    cout << "目前堆疊內容: " << endl;
+    for (int i = 0; i <= top; i++) {
+        cout << stack[i] << " ";
+    }
+    cout << endl;
+
+    if (top >= 0) {
+        cout << "彈出元素: " << stack[top] << endl;
+        top--;
+    } else {
+        cout << "堆疊是空的，不能彈出!" << endl;
     }
 
-    // Second part: Printing triangle pattern using for loops
-    cout << "\nTriangle Pattern:\n";
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "*";
-        }
-        cout << endl;
+    cout << "彈出後堆疊內容: " << endl;
+    for (int i = 0; i <= top; i++) {
+        cout << stack[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
