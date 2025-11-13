6270b944c246cf02ee3af0fb3f685beef7eb7602
s1120378-cloud <s1120378@dmhs.kh.edu.tw>
2025-11-13 14:37:20 +0800

    功課8

--- main.cpp
+++ main.cpp
@@ -1,45 +1,31 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <iostream>
-using namespace std;
+
+double calculateBMI(double weight_kg, double height_cm) {
+    if (height_cm <= 0) return 0.0;
+    
+    double height_m = height_cm / 100.0;  // Convert cm to meters
+    double bmi = weight_kg / (height_m * height_m);  // BMI calculation
+    return bmi;
+}
 
 int main() {
-    int stack[5];
-    int top = -1;  // 堆疊頂端指標
+    double weight1, weight2, height1, height2;
 
-    top++;
-    stack[top] = 10;
+    weight1 = 80;
+    weight2 = 70;
+    height1 = 168;
+    height2 = 188;
 
-    top++;
-    stack[top] = 20;
-
-    top++;
-    stack[top] = 30;
-
-    cout << "目前堆疊內容: " << endl;
-    for (int i = 0; i <= top; i++) {
-        cout << stack[i] << " ";
-    }
-    cout << endl;
-
-    if (top >= 0) {
-        cout << "彈出元素: " << stack[top] << endl;
-        top--;
-    } else {
-        cout << "堆疊是空的，不能彈出!" << endl;
-    }
-
-    cout << "彈出後堆疊內容: " << endl;
-    for (int i = 0; i <= top; i++) {
-        cout << stack[i] << " ";
-    }
-    cout << endl;
+    std::cout << "Your BMI is: " << calculateBMI(weight1, height1) << std::endl;
+    std::cout << "Another BMI is: " << calculateBMI(weight2, height2) << std::endl;
 
     return 0;
 }
